57bd119673f3e9783716e3a21f864bee78ed9dfe
RELIQ20 <nathaniellawas05@gmail.com>
2026-02-15 15:09:39 +0800

    colorscheme correction

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,8 +4,13 @@ return {
     name = "catppuccin",
     priority = 1000,
     config = function()
-      auto_integrations = true,
-      vim.cmd.colorscheme "catppuccin"
+      require("catppuccin").setup({
+        flavour = "mocha", -- sets the specific dark theme you like
+        transparent_background = false,
+        term_colors = true,
+      })
+
+      vim.cmd.colorscheme "catppuccin-mocha"
     end,
   },
 }
